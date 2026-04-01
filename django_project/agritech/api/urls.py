4dde25b0128a6940d02b8b7c04388bf0da0c1448
BoneOg <vaughnpereyra098@gmail.com>
2026-04-01 21:26:04 +0800

    moved

--- django_project/agritech/api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('posts/', include('post.urls')),
-]
--- django_project/agritech/post/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SensorDataListCreateAPIView, SensorDataRetrieveUpdateDestroyAPIView
-
-urlpatterns = [
-    path('', SensorDataListCreateAPIView.as_view(), name='sensordata-list-create'),
-    path('<int:pk>/', SensorDataRetrieveUpdateDestroyAPIView.as_view(), name='sensordata-detail'),
-]
